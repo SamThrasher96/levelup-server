f8803bac077efe917e84fce6f7ba64b115067df1
Sam Thrasher <samthrasher9@gmail.com>
2023-08-09 10:18:48 -0500

    Added multiple views

---
+++ levelupapi/views/game_view.py
@@ -0,0 +1,23 @@
+"""View module for handling requests about game types"""
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from levelupapi.models import Game
+
+
+class GameView(ViewSet):
+    def retrieve(self, request, pk):
+        game = Game.objects.get(pk=pk)
+        serializer = GameSerializer(game)
+        return Response(serializer.data)
+
+    def list(self, request):
+        game = Game.objects.all()
+        serializer = GameSerializer(game, many=True)
+        return Response(serializer.data)
+
+class GameSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Game
+        fields = ('id', 'title', 'game_type_id', 'creator_id', 'description')
